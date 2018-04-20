cec0318b90ddbba25a7a17710ba5bc9c28b9ef7a
kchin07 <kchin07@calpoly.edu>
2018-04-19 22:36:09 -0700

    Update intsub.s

--- intsub.s
+++ intsub.s
@@ -5,19 +5,17 @@ intsub:
 		push {r2-r4, lr}
 		mov r2, r0 //r2 = x
 		mov r3, r1 //r3 = y
-		mov r4, #0 //r4 = ret
+        mov r4, #0 //r4 = borrow
 
-		eor r3, r3, #255
+while:
+        cmp r3, #0
+        beq return
+        eor r2, r2, #0xFFFFFFFF
+        and r4, r2, r3
+        eor r2, r2, r3
+        LSL r3, r4, #1
+        b while
 
-		mov r0, #1
-		mov r1, r3
-		bl intadd
-
-		mov r3, r0
-		
-		mov r0, r2
-		mov r1, r3
-		bl intadd
-
-		pop {r2-r4, pc}
+return: mov r0, r2
+        pop {r2-r4, pc}
 
